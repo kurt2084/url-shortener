ffdce54b8b5ede1853057e0362fc3d67f617f77d
kurt2084 <kurt2084@gmail.com>
2021-09-14 16:11:45 +0800

    feat: modify mongoose import

--- models/url.js
+++ models/url.js
@@ -1,17 +1,17 @@
 //  import mongoose
-const mongoose = require('require')
+const mongoose = require('mongoose')
 //  use mongoose schema
 const Schema = mongoose.Schema
 //  define url schema data type
 const urlSchema = new Schema({ 
   inputURL: {
     type: String,
-    required: true
+    required: true,
   },
   randomCode: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 })
 //  export url schema as URL
 module.exports = mongoose.model('URL', urlSchema)
